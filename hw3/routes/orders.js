0c141f669581bb161275c15da962ef7d5cc71c96
Hewlett De lara <hewlett.delara01@gmail.com>
2021-09-20 19:41:03 -0700

    Created Json object and have it open by localhost

--- hw3/routes/orders.js
+++ hw3/routes/orders.js
@@ -7,3 +7,25 @@ router.get('/', function(req, res, next) {
 });
 
 module.exports = router;
+
+// Create an array whose objects have two properties
+const ordersArray = [
+    {
+        toppings: "cherry",
+        quantity: 2
+    },
+    {
+        toppings: "plain",
+        quantity: 6
+    },
+    {
+        toppings: "chocolate",
+        quantity: 3
+    }
+];
+
+// Convert the array to Json format
+const myJsonArray = JSON.stringify(ordersArray);
+
+// Send to the localhost client
+res.send(ordersArray);
